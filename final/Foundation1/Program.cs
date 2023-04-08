1c8e6488e60cb9dc4a27232fe147feb57f3c6ff3
Eli LeBlanc <E1leblanc@outlook.com>
2023-04-07 22:16:19 -0500

    all final done

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -3,7 +3,68 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation1 World!");
+    {  
+       Videos video1 = new Videos();
+        video1._author = "jawed";
+        video1._title  = "Me at the zoo";
+        video1._lenght = 19;
+        Comments comment1 = new Comments();
+        comment1._user1 = "Jeff";
+        comment1.commentsList1.Add("This guy is the only person who has the rights to say first on youtube.");
+    
+
+        Videos video2 = new Videos();
+        video2._author = "YouTube";
+        video2._title  = "Youtube Rewind 2018";
+        video2._lenght = 493;
+        Comments comment2 = new Comments();
+        comment2._user2 = "Brandon Butch";
+        comment1.commentsList2.Add("Let's all take a moment of silence for the 19m dislikes before they got removed");
+
+
+
+        Videos video3 = new Videos();
+        video3._author = "officialpsy";
+        video3._title  = "Gangnam Style";
+        video3._lenght = 208;
+        Comments comment3 = new Comments();
+        comment3._user3 = "Sporty kid 101";
+        comment1.commentsList3.Add("The person that first commented on this video must feel like the king of the world.");
+
+
+        
+        List<Videos> videoList = new List<Videos> ();
+        videoList.Add(video1);
+        videoList.Add(video2);
+        videoList.Add(video3);
+
+        int counter = 0;
+        foreach(Videos video in videoList)
+        {
+            video.Display(counter);
+            counter ++;
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("choose a video # to see the comments");
+        string videoNumber = Console.ReadLine();
+
+        if (videoNumber == "0" )
+        {
+            comment1.Display1();
+        }
+        else if (videoNumber == "1" )
+        {
+            comment1.Display2();
+        }
+        else if (videoNumber == "2" )
+        {
+          comment1.Display3();   
+        }
+
+
+        Console.ReadLine();
+
+
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        
+        Order order1 = new Order();
+        Customer newCustomer = new Customer();
+        Products newProduct = new Products();
+        Address newAddress = new Address();
+
+        Console.WriteLine("Welcome to the order machine!");
+
+
+        Console.WriteLine("Let´s add your shipping info now!");
+        Console.WriteLine();
+        Console.WriteLine("What is your name?");                            
+        string customerName = Console.ReadLine();                          
+        newCustomer.SetCustName(customerName);
+
+        Console.WriteLine("What is your street?");                          
+        string street = Console.ReadLine();                                 
+        newAddress.SetStreet(street);
+                
+        Console.WriteLine("What is your city?");                            
+        string city = Console.ReadLine();
+        newAddress.SetCity(city);
+
+        Console.WriteLine("What is your state/province?");                  
+        string state = Console.ReadLine();
+        newAddress.SetState(state);
+
+        Console.WriteLine("What is your Country?");
+        Console.WriteLine("If you are from USA, please tipe -USA- other wise type your country");
+        string country = Console.ReadLine();
+        newAddress.SetCountry(country);
+
+        Console.WriteLine("Great! Address is added!");
+        Console.WriteLine();
+
+        string address = newAddress.GetAddress();                           
+        newCustomer.SetCustAddr(address);
+
+        string question = "";
+        while (question.ToLower() != "no")
+        {
+            Console.WriteLine("Want to order a new product? (Yes/No)");
+            question = Console.ReadLine();
+            if (question.ToLower() == "yes")
+            {
+                
+                Console.WriteLine("Add the Id of the product");                     
+                string newOrderId = Console.ReadLine(); 
+                int newOrderID = int.Parse(newOrderId);
+                newProduct.SetProductId(newOrderID);
+
+                Console.WriteLine("Add the name of the product");                  
+                string newOrderName = Console.ReadLine();   
+                newProduct.SetProductName(newOrderName);
+
+                Console.WriteLine("Add the price of the product");                  
+                string newOrderpriceString = Console.ReadLine(); 
+                float newOrderprice = float.Parse(newOrderpriceString);
+                newProduct.SetProductPrice(newOrderprice);
+
+                Console.WriteLine("How many will you purchase?");                   
+                string newOrderQuantityString = Console.ReadLine(); 
+                int newOrderQuantity = int.Parse(newOrderQuantityString);
+                newProduct.SetProductQuantity(newOrderQuantity);
+                order1.productList.Add(newProduct);          
+            }
+                
+                Console.WriteLine("Please click enter to get the order summary");
+                Console.ReadLine();
+
+                order1.DisplayOrder(newCustomer, newProduct, newAddress );
+                
+                Console.ReadLine();
+            
+        }
+        Console.WriteLine("Your order has been completed!");
     }
 }
